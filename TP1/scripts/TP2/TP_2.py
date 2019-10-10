6ffa8bda3a2e099805f3fce15d5654dc3b3d36be
nathan couton <nathcouton@gmail.com>
2019-10-10 22:54:35 +0200

    calculatrice avec opération et égal en V1

--- TP1/scripts/TP2/TP_2.py
+++ TP1/scripts/TP2/TP_2.py
@@ -1,33 +1,83 @@
 from tkinter import *
 
 
-def calc(ca):
-    if ca == "0":
-        return True
+def AC_function(label, label2):
+    label.config(text="")
+    label2.config(text="")
+
+
+def C_function(label, label2):
+    label.config(text="")
+    label2.config(text="")
+
+
+def addition(label, label2):
+    if label.cget("text") != "":
+        label2.config(text=label2.cget("text") + label.cget("text") + "+")
+        label.config(text="")
+
+
+def equal_function(label, label2):
+    label2_txt = str(label2.cget("text"))
+    if label2_txt.endswith('+') or label2_txt.endswith('-') or label2_txt.endswith('*') or label2_txt.endswith('/'):
+        label2.config(text=label2_txt + label.cget("text"))
+        label.config(text="")
+    label2_txt = str(label2.cget("text"))
+    label.config(text=eval(label2_txt))
+    label2.config(text=label2.cget("text") + "=")
 
 
 
-if __name__ == '__main__':
+fenetre = Tk()
 
-    fenetre = Tk()
+buttons_frame = Frame(fenetre)
+buttons_frame.grid(row=0, column=0, sticky="ewns")
 
-    Label(fenetre, text="0", bg="white", width=20, height=5, borderwidth=5, font=2).grid(row=0, column=0, columnspan=5,
-                                                                                         sticky="ew")
-    for ligne in range(3):
-        for colonne in range(3):
-            Button(fenetre, text=3 * ligne + colonne + 1, width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=ligne + 2,
-                                                                                                          column=colonne)
+label = Label(buttons_frame, text="", bg="white", height=2)
+label.grid(row=1, columnspan=4, sticky="ewns")
+label2 = Label(buttons_frame, text="", bg="white", height=2, fg="#ccc")
+label2.grid(row=0, columnspan=4, sticky="ewns")
 
-    button1 = Button(fenetre, text="0", width=20, height=5, borderwidth=5, font=2, command=lambda: print("salut")).grid(
-        row=5, column=0, columnspan=3, sticky="ew")
-    Button(fenetre, text=",", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=1, column=4)
-    Button(fenetre, text="+", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=4, column=4)
-    Button(fenetre, text="-", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=3, column=4)
-    Button(fenetre, text="*", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=2, column=4)
-    Button(fenetre, text="/", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=5, column=4)
+buttonplus = Button(buttons_frame, text="+", command=lambda: addition(label, label2), relief="raised", width=5,
+                    height=3)
+buttonplus.grid(row=2, column=3)
+buttonminus = Button(buttons_frame, text="-", command=lambda: label2.config(text="-"), relief="raised", width=5,
+                     height=3)
+buttonminus.grid(row=3, column=3)
+buttontimes = Button(buttons_frame, text="x", command=lambda: label2.config(text="x"), relief="raised", width=5,
+                     height=3)
+buttontimes.grid(row=4, column=3)
+buttondiv = Button(buttons_frame, text="/", command=lambda: label2.config(text="/"), relief="raised", width=5, height=3)
+buttondiv.grid(row=2, column=2)
 
-    Button(fenetre, text="AC", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=1, column=0)
-    Button(fenetre, text="C", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=1, column=1)
-    Button(fenetre, text="=", width=20, height=5, borderwidth=5, font=2, command=lambda: calc()).grid(row=1, column=2)
+buttondequals = Button(buttons_frame, text="=", command=lambda: equal_function(label, label2), relief="raised", width=5,
+                       height=3)
+buttondequals.grid(row=5, column=3)
+buttonC = Button(buttons_frame, text="C", command=lambda: C_function(label, label2), relief="raised", width=5, height=3)
+buttonC.grid(row=2, column=0)
+buttonAC = Button(buttons_frame, text="AC", command=lambda: AC_function(label, label2), relief="raised", width=5,
+                  height=3)
+buttonAC.grid(row=2, column=1)
 
-    fenetre.mainloop()
+button0 = Button(buttons_frame, text="0", command=lambda: label.config(text="0"), relief="raised", width=5, height=3)
+button0.grid(row=6, column=1)
+button1 = Button(buttons_frame, text="1", command=lambda: label.config(text="1"), relief="raised", width=5, height=3)
+button1.grid(row=5, column=0)
+button2 = Button(buttons_frame, text="2", command=lambda: label.config(text="2"), relief="raised", width=5, height=3)
+button2.grid(row=5, column=1)
+button3 = Button(buttons_frame, text="3", command=lambda: label.config(text="3"), relief="raised", width=5, height=3)
+button3.grid(row=5, column=2)
+button4 = Button(buttons_frame, text="4", command=lambda: label.config(text="4"), relief="raised", width=5, height=3)
+button4.grid(row=4, column=0)
+button5 = Button(buttons_frame, text="5", command=lambda: label.config(text="5"), relief="raised", width=5, height=3)
+button5.grid(row=4, column=1)
+button6 = Button(buttons_frame, text="6", command=lambda: label.config(text="6"), relief="raised", width=5, height=3)
+button6.grid(row=4, column=2)
+button7 = Button(buttons_frame, text="7", command=lambda: label.config(text="7"), relief="raised", width=5, height=3)
+button7.grid(row=3, column=0)
+button8 = Button(buttons_frame, text="8", command=lambda: label.config(text="8"), relief="raised", width=5, height=3)
+button8.grid(row=3, column=1)
+button9 = Button(buttons_frame, text="9", command=lambda: label.config(text="9"), relief="raised", width=5, height=3)
+button9.grid(row=3, column=2)
+
+fenetre.mainloop()
