824f06deea43148acceefc5cca1648ac42c12d1e
nathan couton <nathcouton@gmail.com>
2019-11-18 10:46:26 +0100

    Debut Tp5

---
+++ scripts/TP5/TP_5.py
@@ -0,0 +1,19 @@
+import sqlite3
+import csv
+from scripts.TP5.utils import *
+
+if __name__ == '__main__':
+
+    data_communes = parser('../../resources/communes.csv', 7, 8)
+    data_departments = parser('../../resources/departements.csv', 7, 8)
+    data_region = parser('../../resources/regions.csv', 7, 8)
+
+
+    db = sqlite3.connect('../../resources/database/database.sqlite')
+
+    create_table_communes(db, data_communes)
+    create_table_departements(db, data_departments)
+    create_table_regions(db, data_region)
+
+    db.close()
+
